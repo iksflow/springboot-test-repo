436815d191f9961c9c9378a116b7a5206f5c399d
iksflow <theiksflow@gmail.com>
2021-05-08 22:24:13 +0900

    Initial commit

---
+++ src/main/java/com/iksflow/demospring/AppRunner.java
@@ -0,0 +1,24 @@
+package com.iksflow.demospring;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.core.convert.ConversionService;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AppRunner implements ApplicationRunner {
+
+    @Value("${my.value}")
+    int value;
+    @Autowired
+    ConversionService conversionService;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        System.out.println(conversionService);
+        System.out.println(conversionService.getClass().toString());
+        System.out.println("my value = " + value);
+    }
+}
