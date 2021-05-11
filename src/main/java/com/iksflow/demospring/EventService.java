86ddc6e66f37e9dcab28bef29e4f622a81732255
iksflow <theiksflow@gmail.com>
2021-05-11 23:50:46 +0900

    Study AOP Proxy

--- src/main/java/com/iksflow/demospring/EventService.java
+++ src/main/java/com/iksflow/demospring/EventService.java
@@ -3,4 +3,5 @@ package com.iksflow.demospring;
 public interface EventService {
     void createEvent();
     void publishEvent();
+    void deleteEvent();
 }
---
+++ src/main/java/com/iksflow/demospring/PerfAspect.java
@@ -0,0 +1,19 @@
+package com.iksflow.demospring;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class PerfAspect {
+
+    @Around("execution(* com.iksflow..*.EventService.*(..))")
+    public Object logPerf(ProceedingJoinPoint pjp) throws Throwable {
+        long begin = System.currentTimeMillis();
+        Object retVal = pjp.proceed();
+        System.out.println(System.currentTimeMillis() - begin);
+        return retVal;
+    }
+}
